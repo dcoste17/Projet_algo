d02f956408f38d28df03f097c9f4b44bedc79e38
ManuevaM <manueva.meunier@etu.u-bordeaux.fr>
2022-12-21 19:05:34 +0100

    Add Main code for source and target

--- Projet_algo/src/Main.java
+++ Projet_algo/src/Main.java
@@ -1,5 +1,59 @@
-public class Main {
+import io.jbotsim.core.*;
+import io.jbotsim.core.event.SelectionListener;
+import io.jbotsim.core.event.StartListener;
+import io.jbotsim.ui.JViewer;
+import io.jbotsim.ui.icons.Icons;
+
+public class Main implements SelectionListener, StartListener{
+
+    Topology tp;
+    Node sourceNode;
+    Node targetNode;
+    public Main() {
+        tp = new Topology();
+        tp.setTimeUnit(200); // slow down for visualization
+        tp.setDefaultNodeModel(Connexite.class);
+        tp.addSelectionListener(this);
+        tp.addStartListener(this);
+        new JViewer(tp);
+        tp.start();
+    }
+    @Override
+    public void onSelection(Node node) {
+
+        Node selectedNode = (Node) node;
+
+        //la source devient bleu
+        if (sourceNode == null) {
+            sourceNode = selectedNode;
+            sourceNode.setColor(Color.white);
+
+            //la destination devient noir
+        } else if (targetNode == null){
+            targetNode = selectedNode;
+            targetNode.setColor(Color.black);
+
+        }
+    }
+
+    @Override
+    public void onStart() {
+        if (targetNode != null) {
+            targetNode.setIcon(Icons.DEFAULT_NODE_ICON);
+            targetNode.setIconSize(10);
+            targetNode = null;
+        }
+        sourceNode = null;
+        for (Node node : tp.getNodes())
+            node.setColor(null);
+        for (Link link : tp.getLinks())
+            link.setWidth(1);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main();
     }
 }
+
+
