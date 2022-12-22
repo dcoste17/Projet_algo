55476b783f62ca85d29a46632614f71408370677
dcoste17 <75047349+dcoste17@users.noreply.github.com>
2022-12-22 19:15:30 +0100

    fonction elagage première version

--- Projet_algo/src/Connexite.java
+++ Projet_algo/src/Connexite.java
@@ -1,6 +1,8 @@
 import io.jbotsim.core.Color;
 import io.jbotsim.core.Message;
 import io.jbotsim.core.Node;
+import io.jbotsim.core.Topology;
+import io.jbotsim.core.Link;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +34,84 @@ public boolean est_connexe_sans(Node s, Node t, List<Node> interdit){
     return false;
 }
 
+    public Topology elagage(Node s, Node t) {
+        Stack<Node> pile = new Stack<>();
+        Stack<Node> pile_tmp = new Stack<>();
+        Topology graphe_elage = new Topology();
+        pile.add(s);
+        Node tmp;
+        Node noeud_marqué = s;
+        int modif_tour = 0;
+        int noeuds_ajoute = 0;
+        int nb_voisins = 0;
+        int nb_voisins_niv_suivant = 0;
+        int voisins_visités = 0;
+        while (!pile.empty()) {
+            tmp = pile.pop();
+            List<Node> voisins = tmp.getNeighbors();
+            nb_voisins = s.getNeighbors().size();
+            do {
+                if (modif_tour != 0) {
+                    int position = 0;
+                    for (Node v : voisins) {
+                        if (position+1 == nb_voisins && !pile.contains(noeud_marqué)){
+                            position++;
+                            noeud_marqué = v;
+                        }
+                        if (!visited.contains(v) || !pile.contains(v) || v!=t){
+                            pile.add(v);
+                            //pile_tmp.add(v);
+                        }
+                        //si le noeud correspond au noeud de départ, on garde le lien
+                        if (tmp == s) {
+                            Link lien = new Link(tmp, v);
+                            graphe_elage.addNode(v);
+                            graphe_elage.addLink(lien);
+                            modif_tour++;
+                            noeuds_ajoute++;
+                        } else {
+                            //si le noeud voisin correspond au noeud d'arrivée, on garde le lien
+                            if (v == t) {
+                                Link lien = new Link(tmp, v);
+                                graphe_elage.addNode(v);
+                                graphe_elage.addLink(lien);
+                                modif_tour++;
+                                noeuds_ajoute++;
+                                nb_voisins_niv_suivant++;
+                            } else {
+                                //si le noeud voisin n'a pas déjà été visité ET qu'il n'est lié qu'à son
+                                //parent + un autre noeud
+                                if (!visited.contains(v) && v.getNeighbors().size() == 2) {
+                                    //si un voisin du noeud voisin est déjà visité, on n'ajoute pas le noeud
+                                    for (Node v_visited : visited) {
+                                        if (v.hasNeighbor(v_visited)) {
+                                            break;
+                                        }
+                                        Link lien = new Link(tmp, v);
+                                        graphe_elage.addNode(v);
+                                        graphe_elage.addLink(lien);
+                                        modif_tour++;
+                                        noeuds_ajoute++;
+                                        nb_voisins_niv_suivant++;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                    visited.add(tmp);
+                } else {
+                    Node choix = voisins.get(0);
+                    Link lien = new Link(tmp, choix);
+                    graphe_elage.addNode(choix);
+                    graphe_elage.addLink(lien);
+                    modif_tour++;
+                    noeuds_ajoute++;
+                }
+            } while (Math.min(nb_voisins, nb_voisins_niv_suivant) != noeuds_ajoute || pile.contains(noeud_marqué));
+        }
+        return graphe_elage;
+    }
 
 
     @Override
